13859ff0f5c32c82c4efdadfa3262eeecdc1c4b8
marceloslo <marceloslo@users.noreply.github.com>
2021-11-16 12:56:20 -0300

    update

--- backend/src/routes/pets/pets.js
+++ backend/src/routes/pets/pets.js
@@ -1,50 +1,25 @@
 const express = require('express');
-const router = express.Router();
+const bodyParser = require('body-parser');
+const Sequelize=require('sequelize');
+const path = require('path');
+const petRoutes=require('../../services/pets/index')
+//connect to database
 const db = require('../../config/pets/database');
-const models = require('../../models/index');
+db.authenticate().then(()=>console.log('Conectado')).catch(err => console.log('error'+err));
 
-const Pet=models.Pet;
-const Comment=models.Comment;
-//finds all pets that match the query
-//send them if exists and 404 if no pet is found
-//else, logs an error
-router.get('/',(req,res)=>{ 
-    Pet.findAll({
-        where: req.query
-    })
-    .then(pets =>{
-        if (pets.length === 0)
-        {
-            res.status(404).send('404 not found');
-            return;
-        }
-        res.send(pets);
-    })
-    .catch(err => console.log('Error: ',err));
+const app= express();
+app.use(express.json());
+
+const PORT = process.env.PORT||5000;
+
+
+//route for all pets (or queried pets)
+app.get('/pets',(req,res) => {petRoutes.getAll(req,res)});
+
+//route for specific id
+app.get('/pets/:id',(req,res) => {petRoutes.getOne(req,res)});
+
+
+app.listen(PORT, function(){
+    console.log(`Listening on ${PORT}`);
 });
-
-//finds pets with specific ids, and their respective comments, and send them if found
-//if not found, sends 404
-//if failed to connect to db/other error, logs an error
-router.get('/:id',async(req,res)=>{
-    const pets=await Pet.findByPk(parseInt(req.params.id))
-    if(!pets)
-    {
-        res.status(404).send('404 not found');
-        return;
-    }
-    var comments=await Comment.findAll({
-        where:{
-            pet_id:parseInt(req.params.id),
-        }
-    });
-    if(!comments)
-    {
-        res.send(pets);
-        return;
-    }
-    comments.unshift(pets);
-    res.send(comments);
-});
-
-module.exports = router;

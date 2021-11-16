faa2f7e6f8b3c0a11e3ad00f3bd7cfbc90912262
marceloslo <marceloslo@users.noreply.github.com>
2021-11-16 13:01:08 -0300

    Update pets.js

--- backend/src/routes/pets/pets.js
+++ backend/src/routes/pets/pets.js
@@ -7,19 +7,15 @@ const petRoutes=require('../../services/pets/index')
 const db = require('../../config/pets/database');
 db.authenticate().then(()=>console.log('Conectado')).catch(err => console.log('error'+err));
 
-const app= express();
-app.use(express.json());
-
-const PORT = process.env.PORT||5000;
+const router= express.Router();
 
 
 //route for all pets (or queried pets)
-app.get('/pets',(req,res) => {petRoutes.getAll(req,res)});
+router.get('/pets',(req,res) => {petRoutes.getAll(req,res)});
 
 //route for specific id
-app.get('/pets/:id',(req,res) => {petRoutes.getOne(req,res)});
+router.get('/pets/:id',(req,res) => {petRoutes.getOne(req,res)});
 
 
-app.listen(PORT, function(){
-    console.log(`Listening on ${PORT}`);
-});
+
+module.exports=router;

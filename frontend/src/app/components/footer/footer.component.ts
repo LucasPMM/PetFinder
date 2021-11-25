a27b89495286b2b79e1c894b8f7cd4b958a9296a
Lucas Mariz <lucaspaulom@hotmail.com>
2021-11-25 09:43:51 -0300

    Adding footer component

---
+++ frontend/src/app/components/footer/footer.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from "@angular/core";
+import {
+  faGithub,
+  faFacebook,
+  faTwitter,
+  faGoogle,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+
+@Component({
+  selector: "app-footer",
+  templateUrl: "./footer.component.html",
+  styleUrls: ["./footer.component.scss"],
+})
+export class FooterComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}

8a7afd1a463936e72c5b0bde349f8793b16a4c4c
LucasPMM <lucaspaulom@hotmail.com>
2021-12-08 14:45:08 -0300

    Showing pets ids

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,21 +1,33 @@
 import { Component, OnInit } from "@angular/core";
 import { Store } from "@ngrx/store";
-import { PetService } from "src/app/services/pets/pets.service";
+import { Observable } from "rxjs";
+import { PetItem } from "src/app/models/pets";
 import { logout } from "src/app/stores/auth/auth.actions";
+import { petsRequested } from "src/app/stores/pets/pets.actions";
+import { getPetsList } from "src/app/stores/pets/pets.selectors";
 import { AppState } from "src/app/stores/reducers";
 
 @Component({
   selector: "app-dashboard",
   templateUrl: "./dashboard.component.html",
   styleUrls: ["./dashboard.component.scss"],
-  providers: [PetService],
+  providers: [],
 })
 export class DashboardComponent implements OnInit {
   constructor(private store: Store<AppState>) {}
+
+  public pets$: Observable<PetItem[]>;
 
   public logout() {
     this.store.dispatch(logout({}));
   }
 
-  ngOnInit(): void {}
+  public getPets() {
+    this.store.dispatch(petsRequested({}));
+  }
+
+  ngOnInit(): void {
+    this.pets$ = this.store.select(getPetsList);
+    this.getPets();
+  }
 }

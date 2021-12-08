93a47c0045f2d3716f92217155ef9f62621c3aed
LucasPMM <lucaspaulom@hotmail.com>
2021-12-08 14:44:48 -0300

    Adding pets model

---
+++ frontend/src/app/models/pets.ts
@@ -0,0 +1,12 @@
+export interface PetItem {
+  age: number;
+  breed: string;
+  createdAt: string;
+  deletedAt: string;
+  description: string;
+  gender: string;
+  id: number;
+  name: string;
+  species: string;
+  updatedAt: string;
+}

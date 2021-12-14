985fe736e324022bd3ce0aa71bc926c8a0fd3d7f
LucasPMM <lucaspaulom@hotmail.com>
2021-12-14 15:41:12 -0300

    Adding comments component

--- frontend/src/app/components/components.module.ts
+++ frontend/src/app/components/components.module.ts
@@ -4,10 +4,11 @@ import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
 import { RouterModule } from "@angular/router";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { CommentsComponent } from "./comments/comments.component";
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
-  exports: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, CommentsComponent],
+  exports: [HeaderComponent, FooterComponent, CommentsComponent],
   imports: [CommonModule, FontAwesomeModule, RouterModule.forChild([])],
 })
 export class ComponentsModule {}
